64db17b6e2800803502efbff602265adeb7b111d
chenlj <clj76181845@163.com>
2020-11-24 19:38:07 +0800

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,46 @@
+import { createRoutes } from '@/utils/core';
+import BasicLayout from '@/layouts/BasicLayout';
+import UserLayout from '@/layouts/UserLayout';
+
+import Login from './Login';
+import Register from './Register';
+import Page403 from './Pages/403';
+import NotFound from './Pages/404';
+import Page500 from './Pages/500';
+import Blank from './Blank';
+/**
+ * 主路由配置
+ *
+ * path 路由地址
+ * component 组件
+ * indexRoute 默认显示路由
+ * childRoutes 所有子路由
+ * NotFound 路由要放到最下面，当所有路由当没匹配到时会进入这个页面
+ */
+const routesConfig = app => [
+  {
+    path: '/sign',
+    title: '登录',
+    indexRoute: '/sign/login',
+    component: UserLayout,
+    childRoutes: [
+      Login(app),
+      Register(app),
+      NotFound()
+    ]
+  },
+  {
+    path: '/',
+    title: '系统中心',
+    component: BasicLayout,
+    indexRoute: '/dashboard',
+    childRoutes: [
+      Blank(app),
+      NotFound(),
+      Page403(),
+      Page500(),
+    ]
+  }
+];
+
+export default app => createRoutes(app, routesConfig);
