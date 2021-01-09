f3fdcfab27b94674b9e0907144d9c724abe7f0b9
陈里杰 <clj76181845@163.com>
2021-01-09 10:50:23 +0800

    commit

--- src/routes/ApplyActivity/components/index.js
+++ src/routes/ApplyActivity/components/index.js
@@ -1,28 +1,48 @@
 import {connect} from "dva";
 import BaseComponent from "components/BaseComponent";
-import {Col, Layout, Row, Form, message, DatePicker, Button, Input} from "antd";
+import {Col, Layout, Row, Form, message, DatePicker, Button, Input, Upload, InputNumber, Slider} from "antd";
+import { UploadOutlined  } from '@ant-design/icons';
 import React from "react";
 const { Content } = Layout;
 const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 
 let ac = new BMap.Autocomplete({"input" : "position"});
-@connect()
+
+
+@connect(({ applyActivity, loading }) => ({
+    applyActivity,
+    loading: loading.models.applyActivity
+}))
 export default class extends BaseComponent {
     formRef = React.createRef();
+
     constructor() {
         super();
         this.state = {
             position : "",
+            fileList: [],
+            inputValue: 1,
+            url: null
         }
     }
+    onChange = value => {
+        this.setState({
+            inputValue: value,
+        });
+    };
 
-    onFinish = (values) =>{
-        console.log(values)
-        message.success('提交成功')
-    }
+    handleChange = ({ fileList ,event, file}) => {
+
+        if(file.response !== undefined && file.response !== null){
+            this.setState({url: file.response.data.url})
+        }
+        this.setState({fileList})
+    };
+
 
     render() {
-
+        const {  loading, dispatch } = this.props;
         const onFill = (value) => {
             this.formRef.current.setFieldsValue({
                 position : value
@@ -51,42 +71,105 @@ export default class extends BaseComponent {
             onFill(value)
         });
 
+        const { fileList, inputValue } = this.state;
+
+
+        const onFinish = (values) =>{
+            values.people_limit = this.state.inputValue
+
+            values = {
+                ...values,
+                activity_img: this.state.url,
+                startTime: new Date(values.time[0]).getTime(),
+                endTime: new Date(values.time[1]).getTime()
+            }
+            console.log(values)
+            dispatch({
+                type: 'applyActivity/createActivity',
+                payload: {
+                    values
+                }
+            });
+
+        }
         return (
             <Layout className="full-layout page blank-page">
                 <Content>
-                    <Row gutter={20}>
-                        <Col offset={4} md={16}>
-                            <Form
-                                labelCol={{ span: 4 }}
-                                wrapperCol={{ span: 14 }}
-                                layout="horizontal"
-                                ref={this.formRef}
-                                onFinish={this.onFinish}
+                    <Form
+                        labelCol={{ span: 4 }}
+                        wrapperCol={{ span: 14 }}
+                        layout="horizontal"
+                        ref={this.formRef}
+                        onFinish={onFinish}
+                    >
+                    <Row gutter={10}>
+                        <Col md={11}>
+                            <Form.Item label="活动名称" name="activityName">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="承办学院" name="collegeName">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="负责人学号" name="auditor_id">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="地点" name="position" id="position">
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="起止时间" name="time">
+                                <RangePicker showTime />
+                            </Form.Item>
+
+                        </Col>
+                        <Col md={13}>
+                            <Form.Item label="活动图片" name="activityImg">
+                            <Upload
+                                action="http://localhost//activity/minio/upload"
+                                accept="image/jpeg"
+                                fileList={fileList}
+                                listType="picture"
+                                onChange={this.handleChange}
                             >
-                                <Form.Item label="活动名称" name="activityName">
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item label="承办学院" name="collegeName">
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item label="负责人" name="directorName">
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item label="地点" name="position" id="position">
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item label="起止时间" name="time">
-                                    <RangePicker showTime />
-                                </Form.Item>
-                                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                                    <Button type="primary" htmlType="submit" >
+                                {fileList.length >= 1 ? null : <Button icon={<UploadOutlined />}>点击上传图片</Button>}
+                            </Upload>
+                            </Form.Item>
+                            <Form.Item label="最大人数" name="people_limit" >
+                                <Row>
+                                    <Col span={12}>
+                                        <Slider
+                                            min={1}
+                                            max={20}
+                                            onChange={this.onChange}
+                                            value={typeof inputValue === 'number' ? inputValue : 0}
+                                        />
+                                    </Col>
+                                    <Col span={4}>
+                                        <InputNumber
+                                            min={1}
+                                            max={20}
+                                            style={{ margin: '0 16px' }}
+                                            value={inputValue}
+                                            onChange={this.onChange}
+                                        />
+                                    </Col>
+                                </Row>
+                            </Form.Item>
+                            <Form.Item label="活动描述" name="activity_intro">
+                                <TextArea placeholder="textarea with clear icon" allowClear style={{height : "145px"}}/>
+                            </Form.Item>
+                        </Col>
+
+                    </Row>
+                        <Row>
+                            <Col>
+                                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
+                                    <Button type="primary" htmlType="submit" loading={loading}>
                                         提交
                                     </Button>
                                 </Form.Item>
-                            </Form>
-                        </Col>
-                    </Row>
-
+                            </Col>
+                        </Row>
+                </Form>
                 </Content>
             </Layout>
         )
--- src/routes/ApplyActivity/model/index.js
+++ src/routes/ApplyActivity/model/index.js
@@ -1,6 +1,21 @@
 import modelEnhance from '@/utils/modelEnhance';
+import {createActivity} from '../service'
+import {message} from "antd";
 
 export default modelEnhance({
     namespace: 'applyActivity',
-
+    state: {
+        activity: null
+    },
+    effects: {
+        // 进入页面加载
+        * createActivity({payload}, {call, put, select}) {
+            console.log(payload)
+            const {data, status} = yield call(createActivity,payload.values);
+            console.log(data)
+            if(status){
+                message.success('提交成功')
+            }
+        },
+    },
 });
